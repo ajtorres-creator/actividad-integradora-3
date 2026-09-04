87f5027376c12be95128939d3ec7f1a8c750adc0
ajtorres-creator <aj.torres@eston.ecotec.edu.ec>
2026-09-04 00:22:26 -0500

    Mejora de navegación y consulta de productos

--- views/productos/lista.php
+++ views/productos/lista.php
@@ -7,47 +7,77 @@ $controller = new ProductoController();
 $productos = $controller->listar();
 
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de productos</title>
-    <link rel="stylesheet" href="../../css/estilo.css">
+
+    <link rel="stylesheet" href="../../CSS/estilo.css">
+</head>
 
 <body>
 
     <h1>Productos registrados</h1>
+
     <a href="crear.php">Registrar nuevo producto</a>
+    &nbsp;&nbsp;
+    <a href="tienda.php">Ver tienda</a>
 
     <br><br>
 
-<div class="productos">
+    <table class="tabla-productos">
 
-    <?php while ($producto = $productos->fetch_assoc()) { ?>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Precio</th>
+                <th>Imagen</th>
+            </tr>
+        </thead>
 
-        <div class="producto">
+        <tbody>
 
-            <img src="../../img/<?php echo $producto["imagen"]; ?>" 
-                alt="<?php echo $producto["nombre"]; ?>">
+            <?php while ($producto = $productos->fetch_assoc()) { ?>
 
-            <h2><?php echo $producto["nombre"]; ?></h2>
+                <tr>
 
-            <p><?php echo $producto["descripcion"]; ?></p>
+                    <td>
+                        <?php echo $producto["id"]; ?>
+                    </td>
 
-            <strong>$<?php echo $producto["precio"]; ?></strong>
+                    <td>
+                        <?php echo $producto["nombre"]; ?>
+                    </td>
 
-            <a href="https://wa.me/593999320240?text=Hola,%20quisiera%20pedir%20este%20producto:%20<?php echo urlencode($producto["nombre"]); ?>%20por%20$<?php echo $producto["precio"]; ?>" 
-                class="btn-pedido" 
-                target="_blank">
-                Pedir este producto
-</a>
+                    <td>
+                        <?php echo $producto["descripcion"]; ?>
+                    </td>
 
-        </div>
+                    <td>
+                        $<?php echo $producto["precio"]; ?>
+                    </td>
 
-    <?php } ?>
+                    <td>
+                        <img 
+                            src="../../img/<?php echo $producto["imagen"]; ?>" 
+                            alt="<?php echo $producto["nombre"]; ?>"
+                        >
+                    </td>
 
-</div>
+                </tr>
+
+            <?php } ?>
+
+        </tbody>
+
+    </table>
 
 </body>
+
 </html>
